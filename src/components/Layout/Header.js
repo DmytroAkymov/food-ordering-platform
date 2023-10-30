d2e315badf9ecdf6ad68bd8203ecd616f85fafdc
Dmytro <2dmitri.akimov@gmail.com>
2023-10-30 09:59:09 +0100

    header-cart-button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import suchiImage from '../../assets/sushi.jpg';
 import styles from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
     return (
         <React.Fragment>
             <header className={styles.header}>
                 <h1>Japanese food</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={styles['main-image']}>
                 <img src={suchiImage} alt="suchi" />
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import CartIcon from '../Cart/CartIcon.js';
+import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = () => {
-    return <button></button>
+    return (
+        <button className={styles.button}>
+            <span className={styles.icon}>
+                <CartIcon />
+            </span>
+            <span>Cart</span>
+            <span className={styles.badge}>0</span>
+        </button>
+    );
 };
 
 export default HeaderCartButton;
